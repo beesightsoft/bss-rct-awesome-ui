1dfd2987a38c6e38401113f7102bc868a3a7dbf1
Nhan Cao <nhancv92@gmail.com>
2018-01-31 18:01:06 +0700

    update

--- bee.js
+++ bee.js
@@ -2,7 +2,7 @@
  * @Author: Nhan Cao 
  * @Date: 2018-01-31 13:18:05 
  * @Last Modified by: nhancv92@gmail.com
- * @Last Modified time: 2018-01-31 15:52:03
+ * @Last Modified time: 2018-01-31 17:58:13
  */
 
 const program = require('commander')
@@ -13,29 +13,20 @@ const exec = require('child_process').execSync
 const format = require('string-format-js')
 const version = require('./package.json').version
 
-const prefix = "bee/"
+const prefix = 'bee/'
 const log = console.log
 
 //TODO: Declare options
 program
     .version('0.1.0')
     .option('-n, --new <lib_name>', 'New module', /^[a-zA-Z0-9]{2,50}$/g)
-    .action(function () {
-        log("new")
-    })
     .option('-i, --install <lib_name>', 'Install module', /^[a-zA-Z0-9]{2,50}$/g)
-    .action(function () {
-        log("install")
-    })
     .option('-u, --uninstall <lib_name>', 'Uninstall module', /^[a-zA-Z0-9]{2,50}$/g)
-    .action(function () {
-        log("uninstall")
-    })
     .parse(process.argv)
 
 //TODO: Make help option is default
 if (!process.argv.slice(2).length) {
-    program.outputHelp(chalk.hex('#ED5323'));
+    program.outputHelp(chalk.hex('#ED5323'))
 } else {
     try {
         if (program.install) {
@@ -56,14 +47,10 @@ if (!process.argv.slice(2).length) {
             var libDir = './BeeUi/bee-%s'.format(lib)
             if (fs.existsSync(libDir)) {
                 fs.readdirSync(libDir).forEach(function (file, index) {
-                    var curPath = libDir + "/" + file;
-                    if (fs.lstatSync(curPath).isDirectory()) { // recurse
-                        deleteFolderRecursive(curPath);
-                    } else { // delete file
-                        fs.unlinkSync(curPath);
-                    }
-                });
-                fs.rmdirSync(libDir);
+                    var curPath = '%s/%s'.format(libDir, file)
+                    fs.unlinkSync(curPath)
+                })
+                fs.rmdirSync(libDir)
             }
             print.ok('Module uninstalled: @%s%s'.format(prefix, lib))
         } else if (program.new) {
@@ -79,7 +66,7 @@ if (!process.argv.slice(2).length) {
                 if (fs.existsSync(packageJson)) {
                     var file = require(packageJson)
                     file.name = '@%s%s'.format(prefix, lib)
-                    fs.writeFileSync(packageJson, JSON.stringify(file));
+                    fs.writeFileSync(packageJson, JSON.stringify(file, null, 2), 'utf-8')
                 }
                 print.ok('Module created: @%s%s'.format(prefix, lib))
             } else {
